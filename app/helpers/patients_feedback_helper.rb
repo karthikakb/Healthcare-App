23c27a266dd1ae32c31e92353aff19a286f95d48
Karthika <karthika.kb@spritle.com>
2021-02-09 19:57:13 +0530

    controller name change

---
+++ app/helpers/patients_feedback_helper.rb
@@ -0,0 +1,5 @@
+module PatientsFeedbackHelper
+   def all_locales
+    I18n.available_locales
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)",locale: /#{I18n.available_locales.join("|")}/ do 
-  resources :patient_feedbacks
+  resources :patients_feedback
   root 'staticpage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
